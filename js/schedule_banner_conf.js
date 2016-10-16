d8a1cae8dc197b8205ccf409ba283eb9200e73a7
yamajikousuke <info@ky2000.moo.jp>
2016-10-16 14:23:49 +0900

    update

--- js/schedule_banner_conf.js
+++ js/schedule_banner_conf.js
@@ -8,8 +8,9 @@ var img_base_path = "images/"
 
 //バナー表示のターゲット日時設定
 var schedules = [
-    { 'year':2016, 'month':10, 'day':15, 'hour':11, 'min':54, 'sec':0, 'img':'img01.png', 'url':'url_01'},
-    { 'year':2016, 'month':10, 'day':15, 'hour':11, 'min':54, 'sec':10, 'img':'img02.png', 'url':'url_02'},
-    { 'year':2016, 'month':10, 'day':15, 'hour':11, 'min':54, 'sec':20, 'img':'img03.png', 'url':'url_03'}
+    { 'year':2016, 'month':10, 'day':16, 'hour':13, 'min':50, 'sec':0, 'img':'img02.png', 'url':'url_01'},
+    { 'year':2016, 'month':10, 'day':16, 'hour':13, 'min':53, 'sec':0, 'img':'img03.png', 'url':'url_02'},
+    { 'year':2016, 'month':10, 'day':16, 'hour':14, 'min':10, 'sec':0, 'img':'img04.png', 'url':'url_03'},
+    { 'year':2016, 'month':10, 'day':16, 'hour':14, 'min':20, 'sec':0, 'img':'img05.png', 'url':'url_03'}
 ];
 
